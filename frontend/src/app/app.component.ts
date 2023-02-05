241558f9f0ef4509cd0e4a1cd2189207a34fcb42
Keny Travanino <keny03@unitec.edu>
2023-02-04 19:36:12 -0600

    Add default route login

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
 
 import { UserService } from "./core";
 
@@ -7,7 +8,15 @@ import { UserService } from "./core";
   templateUrl: "./app.component.html",
 })
 export class AppComponent implements OnInit {
+  private _isAuthenticated:boolean = false;
   constructor(private userService: UserService) {}
+
+  get isAuthenticated():boolean{
+    // this.userService.isAuthenticated.subscribe(res => {
+    //   this._isAuthenticated = res
+    // });
+    return this._isAuthenticated;
+  }
 
   ngOnInit() {
     this.userService.populate();
