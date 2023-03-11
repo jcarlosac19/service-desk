d0830f355505927a9fabccf114c8ba944c0c9821
Keny Travanino <keny03@unitec.edu>
2023-03-11 12:45:27 -0600

    Fix comment style

--- frontend/src/app/core/helpers/index.ts
+++ frontend/src/app/core/helpers/index.ts
@@ -1,6 +1,6 @@
 export const isNullOrUndefined = <T>(value: T):boolean => value === null || value === undefined || typeof value === 'undefined';
 export const isNullOrWhitespace = (value: any):boolean => isNullOrUndefined(value) || value?.toString().trim().length === 0;
-export const isFullObject = <T>(value: Object):boolean => typeof value === 'object' && Object.keys(value).length > 0;
+export const isFullObject = (value: any):boolean => !isNullOrWhitespace(value) && typeof value === 'object' && Object.keys(value).length > 0;
 export const hasValue = <T>(value: T[]):boolean => !isNullOrUndefined(value) && value?.length > 0;
 export const formatDate = (date: Date):string => new Date(date).toLocaleDateString('es-ES');
 export const isFullArray = (value:any) => Array.isArray(value) && value.length > 0 && !isNullOrUndefined(value);
